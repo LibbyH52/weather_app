adf85cf464119f0ae33c7f896eb45d2c90b41fe2
Libby Henry <libbh52@gmail.com>
2019-12-15 21:27:20 +0000

    Secured http request

--- static/js/forecast.js
+++ static/js/forecast.js
@@ -2,7 +2,7 @@ const apikey = 'wXjUBLYnAAAVlJ21hy4Go80N3NaWn7NA';
 const city = document.querySelector('.changeLocation[input]');
 
 const getWeather = async(locationKey) => {
-    const base = `http://dataservice.accuweather.com/currentconditions/v1/`;
+    const base = `https://dataservice.accuweather.com/currentconditions/v1/`;
 
     const query = `${locationKey}?apikey=${apikey}`;
 
@@ -14,7 +14,7 @@ const getWeather = async(locationKey) => {
 //get city info
 const getCity = async(city) => {
     //base url of api endpoing that want to make a request to
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const base = 'https://dataservice.accuweather.com/locations/v1/cities/search';
 
     const query = `?apikey=${apikey}&q=${city}`;
 
